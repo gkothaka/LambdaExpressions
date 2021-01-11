efe6c62fadaf3f6597dc0c1a665b10e81659b970
Gouthami Kothakapu <gouthamikothakapu111@gmail.com>
2021-01-10 18:33:07 -0600

    indent

--- src/lambda/ConsumerInterfaceAndForEachLoop.java
+++ src/lambda/ConsumerInterfaceAndForEachLoop.java
@@ -7,19 +7,18 @@ public class ConsumerInterfaceAndForEachLoop {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		List<Integer> obj = Arrays.asList(1,2,3,4);
-		//Traditional for loop
-		for(int i=0; i<obj.size(); i++)
-		{
+		List<Integer> obj = Arrays.asList(1, 2, 3, 4);
+		// Traditional for loop
+		for (int i = 0; i < obj.size(); i++) {
 			System.out.println(obj.get(i));
 		}
-		//Enhanced For Loop to print values
-		for(int i : obj)
-		{
+		// Enhanced For Loop
+		for (int i : obj) {
 			System.out.println(i);
 		}
-		//ForEach Loop (supported in java 1.8)
-		//For each method accepts the value of consumer Interface, with in which accept method should be implemented
+		// ForEach Loop (supported in java 1.8)
+		// For each method accepts the value of consumer Interface, with in which accept
+		// method should be implemented
 		obj.forEach(i -> System.out.println(i));
 	}
 }
